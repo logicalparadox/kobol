28cc6b7267a1735ffd608d9e5a27028bd9fad01e
Jake Luer <jake.luer@gmail.com>
2011-07-02 17:48:54 -0400

    sticky js working

--- js/kobol-stickynav.js
+++ js/kobol-stickynav.js
@@ -1,11 +1,15 @@
 jQuery(function($) {
-  
   $(document).ready(function() {
+     var $nav = $('nav#access');
+         navOffset = $nav.offset();
+     $nav.addClass('sticky-nav');
      
-     $('nav#access').addClass('sticky-nav');
-     
-     
-     
+     $(window).scroll(function(){
+       if( $(window).scrollTop() >= navOffset.top ) {
+         $nav.addClass('stuck');
+       } else {
+         $nav.removeClass('stuck');
+       }
+     });
   });
-  
 });
